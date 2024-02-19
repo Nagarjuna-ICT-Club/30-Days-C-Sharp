ae10d2791747e80dae6245109004b4d6a1fc647b
giver-kdk <giverbro13@gmail.com>
2024-02-19 11:39:25 +0545

    Day 4

---
+++ Day-04/day4.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace PassRef
+{
+	public class Program
+	{
+		static void Modify(int i)
+		{
+			i = i + 5;
+		}
+		// Using 'ref' keyword to pass by reference
+		static void ModifyRef(ref int i)
+		{
+			i = i + 5;
+		}
+		
+		public static void Main(string[] args)
+		{
+			int i = 10;
+			Console.WriteLine("Before: i = " + i);          // Output: 10
+			Modify(i);            // Pass by value   
+			Console.WriteLine("After:  i = " + i);          // Output: 10
+           
+			Console.WriteLine("Before: i = " + i);          // Output: 10
+			ModifyRef(ref i);    // Pass by reference
+			Console.WriteLine("After:  i = " + i);          // Output: 15
+		}
+	}
+}
