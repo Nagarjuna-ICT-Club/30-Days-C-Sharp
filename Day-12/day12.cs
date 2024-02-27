e58a9f8b004ff66b77a9ca904b40002566f49d04
giver-kdk <giverbro13@gmail.com>
2024-02-27 18:03:57 +0545

    Day 12

---
+++ Day-12/day12.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace AbstractClassApp
+{
+	public abstract class Vehicle			// Abstract Class must have atleast one abstract method 
+	{
+		public string name;
+		public Vehicle(string n){
+			name = n;
+		}
+		public abstract void ShowInfo();	// Abstract methods should only be defined by the child class
+	}
+	public class Car : Vehicle
+	{
+		public double speed;
+		public Car(string n, double s) : base(n){
+			speed = s;
+		}
+		public override void ShowInfo(){					// Overwriding abstract method of parent class
+			Console.WriteLine("Car Name: " +  name);
+			Console.WriteLine("Car Speed: " + speed);
+		}
+	}
+	public class Program
+	{
+		public static void Main(string[] args){
+			//Vehicle v1 = new Vehicle("Buggatti", 380);		// Abstract Class can't be instantiated
+			Car c1 = new Car("BMW", 400);
+			c1.ShowInfo();
+		}
+	}
+}
+
+// If a class is 'abstract' then it means, 
+// It is incomplete and some methods needs to be implemented by child classes to be complete
+// Abstract can't be insatnciated but can have contructor for setting variables 
+// It is only supposed to be a base class for other classes to inherit it
