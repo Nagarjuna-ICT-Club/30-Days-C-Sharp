c313c42d1eb161aab78a1857f39af6a693af9986
giver-kdk <giverbro13@gmail.com>
2024-02-28 18:39:09 +0545

    Day 13 Updated

--- Day-13/day13.cs
+++ Day-13/day13.cs
@@ -5,10 +5,10 @@ namespace IndexerApp
 	public class Students
 	{
 		public string[] names = new string[3];
-		// 'Indexer' is similar to a 'Property' but, it takes parameter		
-		public string this[int i]			// This indexer returns the 'ith' string element of string array
+		// This indexer returns the 'ith' string element of string array
+		public string this[int i]           
 		{
-			get {return names[i];}
+			get { return names[i]; }
 			set{
 				if (!names.Contains(value)) names[i] = value;
 				else names[i] = "No Name";
@@ -17,19 +17,17 @@ namespace IndexerApp
 	}
 	public class Program
 	{
-		public static void Main(string[] args){
+		public static void Main(string[] args)
+		{
 			Students s1 = new Students();
-			s1[0] = "Ram";				// Set object's array member as if object is the array itself
+			s1[0] = "Ram";        // Set object's array member as if object is the array itself
 			s1[1] = "Hari";
-			s1[2] = "Hari";             // 'Hari' already exists. So, this becomes 'null'
-
-			foreach(string name in s1.names)				// Get object's array members
+			s1[2] = "Hari";       // 'Hari' already exists. So, this becomes 'null'
+			// Output: Ram, Hari, No Name,
+			foreach (string name in s1.names)               // Get object's array members
 			{
-				Console.WriteLine(name);	
+				Console.Write(name + ", ");
 			}
 		}
 	}
 }
-
-// Indexer are basically parameterized Property but they use 'this[]' keyword instead of an indentifier 
-// 'Indexer' is also known as 'Smart Array'
