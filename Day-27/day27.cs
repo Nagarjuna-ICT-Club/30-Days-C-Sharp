39259da52c5a2aca21936645a59cc4cb9eb875bf
giver-kdk <giverbro13@gmail.com>
2024-03-15 13:44:42 +0545

    Day 27

---
+++ Day-27/day27.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace LambdaDelegate
+{
+	public delegate int MyDelegate(int value);			// Delegate
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Statement Lambda allows to write multiple statements
+			MyDelegate md2 = (x) =>
+			{
+				Console.Write("Square Value is: ");
+				return x * x;
+			};
+			int square = md2(5);
+			Console.WriteLine(square);
+
+			// Expression Lambda allows to returns result in one line without 'return' keyword
+			MyDelegate md = x => x * x;
+
+			square = md(10);
+			Console.WriteLine(square);
+		}
+	}
+}
